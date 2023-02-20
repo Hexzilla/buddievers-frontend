89c2082f93361f2ed5640d6b8f59af5ead54cc7f
Pavlo <corasphinxy@gmail.com>
2023-02-20 14:47:18 +0900

    update About Section

--- src/views/AboutSection.vue
+++ src/views/AboutSection.vue
@@ -2,45 +2,50 @@
   <div class="has-text-centered">
     <div class="columns">
       <div class="column is-6 p-4">
-        <p class="font-size-96-green">ABOUT</p>
-        <p class="font-size-40 mt-3">An NFT project building the utopia.</p>
-        <p class="font-size-24 mt-5">
-          Launched in April 2022, BUDDIES quickly evolved into one of the most
-          successful collections on Kusama Network with just 50 NFTs.
-        </p>
-        <p class="font-size-24 mt-3">
-          BUDDIES have built a solid economic backing by collecting assets and
-          through Alpha Trading, thereby proving that it is one of the most
-          successful NFT collections in the Polkadot ecosystem. BUDDIES have 6
-          core value propositions: Art, Value, Utility, Profitability, Education
-          & Fun.
-        </p>
-        <p class="font-size-24 mt-3">
-          Now, we are working toward our multi-chain future as we will debut our
-          second generation NFT collection on Moonsama network. Moonbuddies is
-          the final generation of the BUDDIES NFT collection, but just the
-          beginning for our community goals.
-        </p>
-        <div class="columns mt-3 mb-3 is-mobile">
-          <div class="column is-1"></div>
+        <div class="columns">
+          <div class="column is-1 p-4"></div>
+          <div class="column is-11 p-4">
+            <p class="font-size-96-green">ABOUT</p>
+            <p class="font-size-40 mt-3">An NFT project building the utopia.</p>
+            <p class="font-size-24 mt-5">
+              Launched in April 2022, BUDDIES quickly evolved into one of the
+              most successful collections on Kusama Network with just 50 NFTs.
+            </p>
+            <p class="font-size-24 mt-3">
+              BUDDIES have built a solid economic backing by collecting assets
+              and through Alpha Trading, thereby proving that it is one of the
+              most successful NFT collections in the Polkadot ecosystem. BUDDIES
+              have 6 core value propositions: Art, Value, Utility,
+              Profitability, Education & Fun.
+            </p>
+            <p class="font-size-24 mt-3">
+              Now, we are working toward our multi-chain future as we will debut
+              our second generation NFT collection on Moonsama network.
+              Moonbuddies is the final generation of the BUDDIES NFT collection,
+              but just the beginning for our community goals.
+            </p>
+            <div class="columns mt-3 mb-3 is-mobile">
+              <div class="column is-1"></div>
 
-          <div class="column small-div">
-            <p class="font-size-40-white">50</p>
-            <p class="font-size-30-white">NFTs</p>
-          </div>
+              <div class="column small-div">
+                <p class="font-size-40-white">50</p>
+                <p class="font-size-30-white">NFTs</p>
+              </div>
 
-          <div class="column is-1"></div>
-          <div class="column small-div">
-            <p class="font-size-40-white">36</p>
-            <p class="font-size-20-white">UNIQUE HOLDERS</p>
-          </div>
+              <div class="column is-1"></div>
+              <div class="column small-div">
+                <p class="font-size-40-white">36</p>
+                <p class="font-size-20-white">UNIQUE HOLDERS</p>
+              </div>
 
-          <div class="column is-1"></div>
-          <div class="column small-div">
-            <p class="font-size-40-white">+800</p>
-            <p class="font-size-20-white">$KSM VOLUME</p>
+              <div class="column is-1"></div>
+              <div class="column small-div">
+                <p class="font-size-40-white">+800</p>
+                <p class="font-size-20-white">$KSM VOLUME</p>
+              </div>
+              <div class="column is-1"></div>
+            </div>
           </div>
-          <div class="column is-1"></div>
         </div>
       </div>
       <div class="column is-6">
