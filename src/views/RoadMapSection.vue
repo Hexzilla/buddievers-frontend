63e5b0581f3d146695c85d2f49bfdbf2a8d1406d
Pavlo <corasphinxy@gmail.com>
2023-02-20 16:10:10 +0900

    update Road Map Section

--- src/views/RoadMapSection.vue
+++ src/views/RoadMapSection.vue
@@ -1,197 +1,10 @@
 <template>
-  <div class="pt-6 pl-6 pr-6 pb-0 roadmap-div-3">
-    <p class="font-size-96-green mt-6 has-text-centered">ROADMAP</p>
-    <div class="columns roadmap-iamge-div hide-mobile-view">
-      <div class="column is-2 has-text-centered">
-        <img
-          src="img/roadmap-1 1.png"
-          alt="No Image"
-          class="fix-height-image"
-        />
-      </div>
-      <div class="column has-text-centered">
-        <p class="font-size-128 number-font-transform">01</p>
-      </div>
-      <div class="column has-text-centered">
-        <p class="font-size-48">LAUNCH</p>
-      </div>
-
-      <div class="column is-4">
-        <p class="font-size-24-green white-space-no-wrap">
-          · Launch Twitter and Discord <br />
-          · BUDDIES NFTs listing start <br />
-          · Announcement: weekly CBD raffle
-          <br />
-          · Announcement: Crazy Games
-        </p>
-      </div>
-      <div class="column is-2"></div>
-    </div>
+  <div class="pt-6 roadmap-container">
+    <p class="font-size-96-green m-6 has-text-centered">ROADMAP</p>
     <div class="columns roadmap-iamge-div hide-mobile-view">
       <div class="column is-2"></div>
-      <div class="column is-2 has-text-centered">
-        <img
-          src="img/roadmap-2 1.png"
-          alt="No Image"
-          class="fix-height-image"
-        />
-      </div>
-      <div class="column has-text-centered">
-        <p class="font-size-128 number-font-transform">02</p>
-      </div>
-      <div class="column has-text-centered">
-        <p class="font-size-48 white-space-no-wrap">MID COLLECTION</p>
-      </div>
-
-      <div class="column is-4">
-        <p class="font-size-24-green white-space-no-wrap">
-          · Free merchandise for holders
-          <br />
-          · New tool: Buddies Alpha Trading
-          <br />
-          · Free banner airdrop <br />
-          · BUDDIES Bank creation
-        </p>
-      </div>
-    </div>
-    <div class="columns roadmap-iamge-div hide-mobile-view">
-      <div class="column is-2 has-text-centered">
-        <img
-          src="img/roadmap-3 1.png"
-          alt="No Image"
-          class="fix-height-image"
-        />
-      </div>
-      <div class="columnhas-text-centered">
-        <p class="font-size-128 number-font-transform">03</p>
-      </div>
-      <div class="column has-text-centered">
-        <p class="font-size-48 white-space-no-wrap">END OF COLLECTION</p>
-      </div>
-
-      <div class="column is-4">
-        <p class="font-size-24-green white-space-no-wrap">
-          · BUDDIES Bank starts working <br />
-          · Free Bong NFT airdrop<br />
-          · Statue raffle<br />
-          · Free banner airdrop<br />
-          · Skybreach & Bit.Country plots acquisition
-        </p>
-      </div>
-      <div class="column is-2"></div>
-    </div>
-    <div class="columns roadmap-iamge-div hide-mobile-view">
-      <div class="column is-2"></div>
-      <div class="column is-2 has-text-centered">
-        <img
-          src="img/roadmap-7 1.png"
-          alt="No Image"
-          class="fix-height-image"
-        />
-      </div>
-      <div class="column has-text-centered">
-        <p class="font-size-128 number-font-transform">04</p>
-      </div>
-      <div class="column has-text-centered">
-        <p class="font-size-48 white-space-no-wrap">DEVELOPMENT PHASE</p>
-      </div>
-
-      <div class="column is-4">
-        <b-button
-          class="
-            font-size-25
-            pr-4
-            pl-4
-            pt-0
-            pb-0
-            normal-button
-            mb-2
-            white-space-no-wrap
-          "
-          >YOU ARE HERE</b-button
-        >
-        <p class="font-size-24-green white-space-no-wrap">
-          · New BUDDIES brand <br />
-          · BUDDIES Economy Bible creation<br />
-          · BUDDIES lore/comic<br />
-          · Free banner airdrop<br />
-          · New tools: alpha, APY hunters, Public Library
-        </p>
-      </div>
-    </div>
-    <div class="columns roadmap-iamge-div hide-mobile-view">
-      <div class="column is-2 has-text-centered">
-        <img
-          src="img/roadmap-4 1.png"
-          alt="No Image"
-          class="fix-height-image"
-        />
-      </div>
-      <div class="columnhas-text-centered">
-        <p class="font-size-128 number-font-transform">05</p>
-      </div>
-      <div class="column has-text-centered">
-        <p class="font-size-48 white-space-no-wrap">MOONBUDDIES</p>
-      </div>
-
-      <div class="column is-4">
-        <p class="font-size-24-green white-space-no-wrap">
-          · Collection launch in Moonsama Network<br />
-          · Launching Buddies web3<br />
-          · Introducing $BUDS token<br />
-          · Moonbank creation<br />
-          · Launching 'The Buddieverse'
-        </p>
-      </div>
-      <div class="column is-2"></div>
-    </div>
-    <div class="columns roadmap-iamge-div hide-mobile-view">
-      <div class="column is-2"></div>
-      <div class="column is-2 has-text-centered">
-        <img
-          src="img/roadmap-6 1.png"
-          alt="No Image"
-          class="fix-height-image"
-        />
-      </div>
-      <div class="column has-text-centered">
-        <p class="font-size-128 number-font-transform">06</p>
-      </div>
-      <div class="column has-text-centered">
-        <p class="font-size-48 white-space-no-wrap">MOON AND BEYOND</p>
-      </div>
-
-      <div class="column is-4">
-        <p class="font-size-24-green white-space-no-wrap">
-          · BUDDIES labs<br />
-          · High Buddies<br />
-          · New utilities inside The Buddieverse
-        </p>
-      </div>
-    </div>
-    <div class="columns roadmap-iamge-div show-mobile-view">
-      <div class="column is-2 has-text-centered">
-        <img
-          src="img/roadmap-1 1.png"
-          alt="No Image"
-          class="fix-height-image"
-        />
-      </div>
-      <div class="column has-text-centered">
-        <p class="font-size-128 number-font-transform">01</p>
-      </div>
-      <div class="column has-text-centered">
-        <p class="font-size-48">LAUNCH</p>
-      </div>
-
-      <div class="column is-4">
-        <p class="font-size-24-green white-space-no-wrap">
-          · Launch Twitter and Discord <br />
-          · BUDDIES NFTs listing start <br />
-          · Announcement: weekly CBD raffle
-          <br />
-          · Announcement: Crazy Games
-        </p>
+      <div class="column is-8">
+        <img src="img/roadstep.png" alt="No Image" class="fix-height-image" />
       </div>
       <div class="column is-2"></div>
     </div>
@@ -215,6 +28,12 @@ export default class RoadMapSection extends Vue {}
   
 <style lang="scss">
 @import "~bulma/sass/utilities/_all";
+
+.roadmap-container {
+  background: url("@/assets/roadBackground.png");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
 
 .roadmap-iamge-div {
   display: flex;
