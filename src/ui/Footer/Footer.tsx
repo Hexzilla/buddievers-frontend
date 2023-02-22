d74a0675ebd82d95a04a080237c3260cd2232c11
Pavlo <corasphinxy@gmail.com>
2023-02-22 16:52:17 +0900

    update Footer|

--- src/ui/Footer/Footer.tsx
+++ src/ui/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Typography, Grid } from '@mui/material';
+import { Typography, Grid, TextField } from '@mui/material';
 import Container from '@mui/material/Container';
 import { useClasses } from 'hooks';
 import { Telegram, Twitter } from 'icons';
@@ -16,24 +16,58 @@ export const Footer = () => {
       <Grid container spacing={0}>
         <Grid item xs={5}>
           <p className={title}>QUICK LINKS</p>
-          <div>
-            <p className={text}>BUDDIES</p>
-            <p className={text}>MOON BUDDIES</p>
-            <p className={text}>About</p>
-          </div>
-          <div>
-            <p className={text}>Marketplace</p>
-            <p className={text}>Coffee Shop</p>
-            <p className={text}>Raresama</p>
-          </div>
+          <Grid container spacing={0}>
+            <Grid item xs={6}>
+              <p className={text}>BUDDIES</p>
+              <p className={text}>MOON BUDDIES</p>
+              <p className={text}>About</p>
+            </Grid>
+            <Grid item xs={6}>
+              <p className={text}>Marketplace</p>
+              <p className={text}>Coffee Shop</p>
+              <p className={text}>Raresama</p>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs={2}>
+        <Grid item xs={2}
+          style={{ alignSelf: "center", textAlign: "center" }}
+        >
           <img
             src="./B-BUDS 1.png"
+            style={{ width: "50%", height: "50%" }}
           />
         </Grid>
-        <Grid item xs={5}>
+        <Grid item xs={5}
+          style={{ alignSelf: "center", textAlign: "center" }}
+        >
           <p className={title}>SIGN OUR NEWSLETTER</p>
+          <TextField id="outlined-basic" label="Email Address"
+            variant="outlined"
+            inputProps={{
+              style: {
+                color: "white",
+                fontSize: "23px"
+              }
+            }} />
+          <Grid container spacing={0}>
+            <Grid item xs={2}>
+            </Grid>
+            <Grid item xs={4}>
+              <p className={title}>JOIN US</p>
+            </Grid>
+            <Grid item xs={2} style={{ alignSelf: "center" }}>
+              <img
+                src="./twitter.png"
+              />
+            </Grid>
+            <Grid item xs={2} style={{ alignSelf: "center" }}>
+              <img
+                src="./discord.png"
+              />
+            </Grid>
+            <Grid item xs={2}>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Container>
