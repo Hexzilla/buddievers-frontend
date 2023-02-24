3030082549ae3c2428c018dc3c53c9ab364ab83c
Pavlo <corasphinxy@gmail.com>
2023-02-24 02:31:25 +0000

    add validation for amount change

--- src/pages/freshtrades/index.tsx
+++ src/pages/freshtrades/index.tsx
@@ -1,4 +1,8 @@
+import { useState, Fragment } from 'react';
 import { useActiveWeb3React, useClasses } from 'hooks';
+import { Container } from '@mui/system';
+import { TextField, Snackbar, IconButton, Grid } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import { Button, GlitchText } from 'ui';
 import { styles } from './styles';
 import { useMint1Contract } from 'hooks/useContracts/useContracts'
@@ -6,11 +10,30 @@ import { MINT1_ADDRESS, ChainId, GAS_LIMIT, MINT_PRICE } from '../../constants'
 
 const FreshTradesPage = () => {
   const { account, error } = useActiveWeb3React();
-
+  const [mintAmount, setMintAmount] = useState(0);
+  const [open, setOpen] = useState(false);
   const mintContract = useMint1Contract(MINT1_ADDRESS[ChainId.MOONRIVER], true);
   const { container, button } = useClasses(styles);
+
+  const handleInputChange = (event: any) => {
+    setMintAmount(event.target.value)
+  }
+
+  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   const mintNFT = async () => {
-    const res = await mintContract?.whitelistedMints(1,
+    if (mintAmount <= 0 || mintAmount > 5) {
+      setOpen(true)
+      return;
+    }
+
+    const res = await mintContract?.whitelistedMints(mintAmount,
       "https://drive.google.com/file/d/1A739BEoj7eU1GnuEdqoWt7AOPtypYN1u/view?usp=share_link", {
       gasLimit: GAS_LIMIT,
       from: account,
@@ -20,16 +43,53 @@ const FreshTradesPage = () => {
     })
   }
 
+  const action = (
+    <Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </Fragment>
+  );
+
   return (
     <>
       <div className={container}>
         <GlitchText variant="h1">Mint NFT</GlitchText>
-        <Button
-          className={button}
-          onClick={mintNFT}
-        >
-          Mint
-        </Button>
+        <Grid container spacing={1}>
+          <Grid item xs={2}>
+            <TextField
+              id="filled-number"
+              label="Mint amounts"
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="filled"
+              size="small"
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <Button
+              className={button}
+              onClick={mintNFT}
+            >
+              Mint
+            </Button>
+          </Grid>
+        </Grid>
+        <Snackbar
+          open={open}
+          autoHideDuration={3000}
+          onClose={handleClose}
+          message="Please input between 1 and 5"
+          action={action}
+        />
       </div>
     </>
   );
