f408a2f18f1bce142192f1efa0929306dd4d0566
Pavlo <corasphinxy@gmail.com>
2023-02-27 20:35:54 +0000

    fix build error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -85,7 +85,7 @@ const EXPLORER_PREFIXES: { [chainId in ChainId]: string } = {
   56: 'bscscan.com',
   246: 'explorer.energyweb.org',
   73799: 'volta-explorer.energyweb.org',
-  5: 'moonriver.moonscan.io',
+  97: 'moonriver.moonscan.io',
   1284: 'moonbeam-rpc.moonsama.com'
 };
 
