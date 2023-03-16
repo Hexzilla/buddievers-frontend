0ccbe7520e25ce404cb60755bfb4eb21d92aba85
prodev9433 <angelbarachiel9571@gmail.com>
2023-03-16 18:05:12 +0900

    style changes

--- src/ui/Footer/Footer.styles.ts
+++ src/ui/Footer/Footer.styles.ts
@@ -28,5 +28,27 @@ export const styles = (theme: Theme) => ({
   hypers : {
     textDecoration : "none",
     color : "white"
+  },
+  footerInput : {
+    width : "100%",
+    height : "60px",
+    background : "white",
+    borderRadius : "10px",
+    paddingLeft : "20px",
+    border : "none",
+    "&::placeholder" : {
+      fontStyle : "italic",
+      color : "#F4F4F4",
+      fontSize : "20px"
+    }
+  },
+  footerButton : {
+    width : "100px",
+    background : "rgb(0, 206, 76)",
+    borderRadius : "10px",
+    height : "60px",
+    marginLeft : "-100px",
+    border : "none",
+
   }
 });
