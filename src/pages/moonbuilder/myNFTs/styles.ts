2b47e924058f221e7b5f3078f292f14b894e46a8
TopCodeBeast <maxb47163@gmail.com>
2023-03-21 16:46:03 +0900

    feat: add myNFTs

---
+++ src/pages/moonbuilder/myNFTs/styles.ts
@@ -0,0 +1,50 @@
+
+
+
+export const styles = () => ({
+    container : {
+        paddingTop : "50px",
+        paddingLeft : '5vw',
+        paddingRight : '5vw',
+        background : "#016B43",
+        paddingBottom : "50px",
+        marginTop : "100px"
+    },
+    stakedNFTs : {
+        marginTop : "60px",
+    },
+    stakeTitleLeft  :{
+        fontSize : "40px",
+        fontWeight : "900",
+        color : "white",
+        marginTop : 0
+    },
+    stakeTitleRight : {
+        fontSize : "24px",
+        fontWeight : "900",
+        color : "#00CE4C"
+    },
+    btnUnStake : {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '20px 40px',
+        height: '76px',
+        color : "white",
+        fontSize : "20px",
+        fontWeight : "400",
+        border : "none",
+        background: 'rgba(0, 206, 76, 0.6)',
+        borderRadius: '20px',
+        width : "100%",
+        cursor : "pointer"
+    },
+    cardMiddle : {
+        width : '100%',
+        paddingTop : "20px",
+        paddingBottom : "20px",
+        paddingLeft : "50px",
+        marginTop : "-6px",
+    }  
+})
