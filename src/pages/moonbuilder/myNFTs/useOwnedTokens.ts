91066d7a397e38856dbcbb6d420110ef480d46d0
reactomoss <reactomoss@gmail.com>
2023-03-30 08:21:19 +0300

    add hook - useOwnedToken

---
+++ src/pages/moonbuilder/myNFTs/useOwnedTokens.ts
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import request from 'graphql-request';
+import { useActiveWeb3React } from 'hooks';
+import { QUERY_OWNED_TOKENS } from 'subgraph/erc721Queries';
+import uriToHttp from 'utils/uriToHttp';
+import {
+  ChainId,
+  CONTRACT_ADDRESS,
+  RARESAMA_SUBGRAPH_URLS,
+} from '../../../constants';
+import { OwnedToken, OwnedTokenPayload } from '../types';
+
+export const getOwnedTokens = async (account: string) => {
+  const address = account;
+  const result: any = await request<OwnedTokenPayload>(
+    RARESAMA_SUBGRAPH_URLS[ChainId.EXOSAMA],
+    QUERY_OWNED_TOKENS(CONTRACT_ADDRESS, address)
+  );
+
+  if (result?.tokens && result.tokens.length > 0) {
+    const tokens = result.tokens.map((token: OwnedToken) => {
+      if (token.metadata?.image) {
+        const urls = uriToHttp(token.metadata.image, true);
+        token.metadata.image = urls[0];
+      }
+      return token;
+    });
+    return tokens;
+  }
+  return [];
+};
+
+const useOwnedTokens = () => {
+  const { account } = useActiveWeb3React();
+  const [tokens, setTokens] = useState<OwnedToken[]>([]);
+
+  useEffect(() => {
+    if (account) {
+      getOwnedTokens(account).then((tokens) => setTokens(tokens));
+    }
+  }, [account]);
+
+  return tokens;
+};
+
+export default useOwnedTokens;
