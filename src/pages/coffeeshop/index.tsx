974ea1c9a36574bf6537eed52cd3eff05510d2b4
reactomoss <reactomoss@gmail.com>
2023-04-08 03:13:14 +0900

    coffeeshop page

--- src/pages/coffeeshop/index.tsx
+++ src/pages/coffeeshop/index.tsx
@@ -1,9 +1,81 @@
-import { useClasses } from "hooks";
+import { useEffect, useMemo, useState } from 'react';
+import { useActiveWeb3React, useClasses } from "hooks";
 import { styles } from './styles';
 import { Grid } from "@mui/material";
+import { NavLink, Button } from 'ui';
+import useOwnedTokens from './useOwnedTokens';
+import {
+  ChainId,
+  CONTRACT_ADDRESS,
+  RARESAMA_SUBGRAPH_URLS,
+} from '../../constants';
+import { QUERY_TOKEN_BY_ID, QUERY_OWNED_PAGE_TOKENS } from 'subgraph/erc721Queries';
+import { OwnedToken, OwnedTokenPayload, PageTokens } from './types';
+import request from 'graphql-request';
+import Pagination from '@mui/material/Pagination';
+import { useWeb3React } from '@web3-react/core';
+import uriToHttp from 'utils/uriToHttp';
+
+
 
 const CoffeeShop = () => {
-    const { container, introContainer, bannerTxtContainer, claimSection, claimButton, rewardMiddleItem, stakedNFTs, stakeTitleLeft, stakeTitleRight, btnUnStake, cardMiddle } = useClasses(styles);
+    const { container, introContainer, bannerTxtContainer, claimSection, claimButton, rewardMiddleItem, stakedNFTs, stakeTitleLeft, stakeTitleRight, btnUnStake, cardMiddle, paginationContainer, paginationStyle } = useClasses(styles);
+    const [countTokens, setCountTokens] = useState<any>(0);
+    const [tokensPage, setTokensPage] = useState<OwnedToken[]>([]);
+    const { account } = useActiveWeb3React();
+    const getPageTokens = async (pageNumber : number, address : string) => {
+        const result: any = await request<PageTokens>(
+            RARESAMA_SUBGRAPH_URLS[ChainId.EXOSAMA],
+            QUERY_OWNED_PAGE_TOKENS(CONTRACT_ADDRESS, address, (pageNumber-1) * 20, 20)
+        );
+        if (result?.tokens && result.tokens.length > 0) {
+            const tokens = result.tokens.map((token: OwnedToken) => {
+            if (token.metadata?.image) {
+                const urls = uriToHttp(token.metadata.image, true);
+                token.metadata.image = urls[0];
+            }
+            return token;
+            });
+            setTokensPage(tokens);
+        }
+        
+    
+    }
+    const pageChangeHandler = async (event : any, pageNumber : number) => {
+        if(account){
+            getPageTokens(pageNumber, account);
+        }
+    
+    }
+    const ownedTokens = useOwnedTokens();
+
+    useEffect(() => {
+        if(account){
+            getPageTokens(1, account);
+        }
+    
+    }, []);
+
+    useEffect(() => {
+        if(account){
+            setCountTokens( ownedTokens.length % 20 == 0 ? ownedTokens.length / 20 : ownedTokens.length / 20 + 1 );
+        }
+    });
+
+    const NFTCards = useMemo(
+        () =>
+        tokensPage.map((token) => (
+            <Grid item md={3} sm={6} >
+                <img src={token.metadata?.image} style={{ width : "100%", height : "400px", borderRadius : "20px" }} />
+                <div className={ cardMiddle }>
+                    <p style={{ fontWeight: 900, fontSize: 24, color: "white", marginBottom : 0 }}>BUDDIE #{token.numericId.toString()}</p>
+                    <p style={{ fontWeight: 400, fontSize: 16, color: "#00CE4C", marginTop : 0 }}>BUDDIES</p>
+                </div>
+                <button className={btnUnStake}>STAKE</button>
+            </Grid>
+        )),
+    [tokensPage, ownedTokens, btnUnStake, cardMiddle]
+    );
     return (
         <div className={ container }>
             <div className={introContainer}>
@@ -91,64 +163,16 @@ const CoffeeShop = () => {
                         <p className={ stakeTitleLeft }>MY NFTS</p>
                     </Grid>
                     <Grid sm={6} style={{ textAlign : "right" }}>
-                        <p className={ stakeTitleRight }>TOTAL OWNED : 6</p>
+                        <p className={ stakeTitleRight }>TOTAL OWNED : {ownedTokens.length}</p>
                     </Grid>
                 </Grid>
                 <Grid container spacing={2}>
-                    <Grid item md={3} sm={6} >
-                        <img src="./charactor (1).png" style={{ width : "100%", height : "400px", borderRadius : "20px" }} />
-                        <div className={ cardMiddle }>
-                            <p style={{ fontWeight: 900, fontSize: 24, color: "white", marginBottom : 0 }}>BUDDIE #08</p>
-                            <p style={{ fontWeight: 400, fontSize: 16, color: "#00CE4C", marginTop : 0 }}>BUDDIES</p>
-                        </div>
-                        <button className={btnUnStake}>STAKE</button>
-                    </Grid>
-                    <Grid item md={3} sm={6} >
-                        <img src="./charactor (2).png" style={{ width : "100%", height : "400px", borderRadius : "20px" }} />
-                        <div className={ cardMiddle }>
-                            <p style={{ fontWeight: 900, fontSize: 24, color: "white", marginBottom : 0 }}>BUDDIE #010</p>
-                            <p style={{ fontWeight: 400, fontSize: 16, color: "#00CE4C", marginTop : 0 }}>BUDDIES</p>
-                        </div>
-                        <button className={btnUnStake}>STAKE</button>
-                    </Grid>
-                    <Grid item md={3} sm={6} >
-                        <img src="./charactor (7).png" style={{ width : "100%", height : "400px", borderRadius : "20px" }} />
-                        <div className={ cardMiddle }>
-                            <p style={{ fontWeight: 900, fontSize: 24, color: "white", marginBottom : 0 }}>BUDDIE #33</p>
-                            <p style={{ fontWeight: 400, fontSize: 16, color: "#00CE4C", marginTop : 0 }}>BUDDIES</p>
-                        </div>
-                        <button className={btnUnStake}>STAKE</button>
-                    </Grid>
-                    <Grid item md={3} sm={6} >
-                        <img src="./charactor (8).png" style={{ width : "100%", height : "400px", borderRadius : "20px" }} />
-                        <div className={ cardMiddle }>
-                            <p style={{ fontWeight: 900, fontSize: 24, color: "white", marginBottom : 0 }}>BUDDIE #37</p>
-                            <p style={{ fontWeight: 400, fontSize: 16, color: "#00CE4C", marginTop : 0 }}>BUDDIES</p>
-                        </div>
-                        <button className={btnUnStake}>STAKE</button>
-                    </Grid>
-                    <Grid container spacing={2} style={{ marginTop : "100px" }}>
-                        <Grid item md={3} sm={6} >
-                            <img src="./charactor (9).png" style={{ width : "100%", height : "400px", borderRadius : "20px" }} />
-                            <div className={ cardMiddle }>
-                                <p style={{ fontWeight: 900, fontSize: 24, color: "white", marginBottom : 0 }}>BUDDIE #36</p>
-                                <p style={{ fontWeight: 400, fontSize: 16, color: "#00CE4C", marginTop : 0 }}>BUDDIES</p>
-                            </div>
-                            <button className={btnUnStake}>STAKE</button>
-                        </Grid>
-                        <Grid item md={3} sm={6} >
-                            <img src="./charactor (10).png" style={{ width : "100%", height : "400px", borderRadius : "20px" }} />
-                            <div className={ cardMiddle }>
-                                <p style={{ fontWeight: 900, fontSize: 24, color: "white", marginBottom : 0 }}>BUDDIE #43</p>
-                                <p style={{ fontWeight: 400, fontSize: 16, color: "#00CE4C", marginTop : 0 }}>BUDDIES</p>
-                            </div>
-                            <button className={btnUnStake}>STAKE</button>
-                        </Grid>
-                        <Grid item md={3} sm={6} ></Grid>
-                        <Grid item md={3} sm={6} ></Grid>
-                    </Grid>
+                    {NFTCards}
                     
                 </Grid>
+                <div className={ paginationContainer }>
+                    <Pagination count={countTokens} onChange={(event, pageNumber) => pageChangeHandler(event, pageNumber)} size="large" shape="circular" showFirstButton showLastButton className={ paginationStyle } />
+                </div>
             </div>
         </div>
     );
--- src/pages/coffeeshop/styles.ts
+++ src/pages/coffeeshop/styles.ts
@@ -94,5 +94,24 @@ export const styles = () => ({
     },
     stakedNFTs : {
         marginTop : "60px",
+    },
+    paginationContainer : {
+        display : "flex",
+        flexDirection : "row",
+        justifyContent : "center",
+        marginTop : "50px"
+    },
+    paginationStyle:{
+        marginTop : "30px",
+        "& > ul > li > button" : {
+            background : "rgba(0, 206, 76, 0.6)",
+            color : "white",
+            borderRadius : "20px",
+            width : "61px",
+            height : "44px",
+        },
+        "& > ul > li > button.Mui-selected" : {
+            backgroundColor : "#00CE4C",
+        }
     }
 })
