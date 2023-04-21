4b5b367518bcd9743b6e48a0af3fa94d1217d8fa
reactomoss <reactomoss@gmail.com>
2023-04-21 19:19:54 +0300

    remove unused imports

--- src/pages/coffeeshop/StakedTokenList.tsx
+++ src/pages/coffeeshop/StakedTokenList.tsx
@@ -1,15 +1,7 @@
-import { useEffect, useMemo, useState, useCallback } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
-import Pagination from '@mui/material/Pagination';
 import { Grid } from '@mui/material';
-import { BigNumber, utils } from 'ethers';
-import { ToastContainer, toast } from 'react-toastify';
-import { useActiveWeb3React, useClasses } from 'hooks';
-import { styles } from './styles';
-import { NavLink, Button } from 'ui';
-import MyNFTs from 'pages/myNFTs';
 import { StakedTokenItem } from '../staking/types';
-import { useStaking } from '../staking/useStaking';
 import StakedToken from './StakedToken';
 
 const EmptyTokens = styled.div`
