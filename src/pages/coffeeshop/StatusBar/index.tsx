f2803301a5f9f36e0984035cc7133e9bae2c8443
reactomoss <reactomoss@gmail.com>
2023-05-05 14:00:38 +0300

    show reward time

--- src/pages/coffeeshop/StatusBar/index.tsx
+++ src/pages/coffeeshop/StatusBar/index.tsx
@@ -1,4 +1,6 @@
+import { useMemo } from 'react';
 import { Grid } from '@mui/material';
+import moment from 'moment';
 
 import { useClasses } from 'hooks';
 import { useStakeContext } from 'context/StakeContext';
@@ -6,8 +8,27 @@ import { useStakeContext } from 'context/StakeContext';
 import { styles } from './styles';
 
 const StatusBar = () => {
-  const { stakedTokens, rewards, claimRewards } = useStakeContext();
+  const { startTime, stakedTokens, rewards, claimRewards } = useStakeContext();
   const { claimSection, claimButton, rewardMiddleItem } = useClasses(styles);
+
+  const nextTime = useMemo(() => {
+    if (stakedTokens?.length) {
+      let sortedTokens = stakedTokens.sort((a, b) => a.timestamp - b.timestamp);
+      let firstToken = sortedTokens[0];
+      let stakedTime = moment(new Date(firstToken.timestamp * 1000));
+
+      let _startTime = moment(new Date(startTime * 1000));
+      let elapsed = moment().diff(_startTime, 'd');
+      let rewardTime = _startTime.add(Math.floor(elapsed) + 1, 'd');
+
+      const diff = rewardTime.diff(stakedTime, 'd');
+      if (diff < 1) {
+        rewardTime = rewardTime.add(1, 'd');
+      }
+      return rewardTime.format('L HH:mm');
+    }
+    return '';
+  }, [startTime, stakedTokens]);
 
   return (
     <div className={claimSection}>
@@ -31,6 +52,10 @@ const StatusBar = () => {
             <Grid item md={3} sm={6} className={rewardMiddleItem}>
               <h3>STAKED</h3>
               <p>{stakedTokens.length} BUDDIES</p>
+            </Grid>
+            <Grid item md={3} sm={6} className={rewardMiddleItem}>
+              <h3>NEXT REWARD</h3>
+              <p>{nextTime}</p>
             </Grid>
           </Grid>
         </Grid>
